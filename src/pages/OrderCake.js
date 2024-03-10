5bedf27d6e6c61186eb183292fb52836ad96c5bf
kariastrand <karinastrand@yahoo.se>
2024-03-10 18:18:10 +0100

    order forms

--- src/pages/OrderCake.js
+++ src/pages/OrderCake.js
@@ -25,6 +25,9 @@ import Img33 from '../Images/WeddingCake8.png';
 import Img34 from '../Images/WeddingCake9.png';
 import Img35 from '../Images/WeddingCake10.png';
 export default function OrderCake() {
+  const [Counter,setCounter]=useState(1)
+  const[selectedOption,setSelectedOption]=useState("vanilla");
+  const[selectedOption2,setSelectedOption2]=useState("10");
   const location=useLocation();
   const data =location.state;
   const {id,type,name,price}=data;
@@ -32,7 +35,16 @@ export default function OrderCake() {
       Img22,Img23,Img24,Img25,Img26,Img27,Img28,Img29,Img30,
       Img31,Img32,Img33,Img34,Img35]
     let src=imgarray[id-16];
+    function onValueChange(event)
+    {
+      setSelectedOption(event.target.value)
+    }
+    function onValueChange2(event)
+    {
+      setSelectedOption(event.target.value)
+    }
     return (
+    
     <div>
       <Layout />
       <div className='order'>
@@ -40,7 +52,7 @@ export default function OrderCake() {
             <img 
               src={src}
               alt="cake"
-              style={{padding:20}}
+              style={{padding:20, maxWidth:750}}
             >
             </img>
             <h3>Description</h3>
@@ -49,13 +61,72 @@ export default function OrderCake() {
             <h2>{name}</h2>
             <h3>{price}</h3>
             <h4>Choose portions</h4>
-            10 portions
+            <label>
+              <input 
+                type='radio' 
+                value="10"
+                  checked={selectedOption2==="10"}
+                onChange={onValueChange2} />
+            
+            10 portions</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="15"
+                checked={selectedOption2==="15"}
+                onChange={onValueChange2} />
+            15 portions</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="30" 
+                checked={selectedOption2==="30"}
+                onChange={onValueChange2} />
+              
+            30 portions</label><br/>
             <h4> Choose filling</h4>
-            vanillia
-            <h4>Message to us</h4>
-            <input></input>
-            Number:<input></input>
-            <button>Place in cart</button>
+            <label>
+              <input 
+                type='radio' 
+                value="vanilla"
+                  checked={selectedOption==="vanilla"}
+                onChange={onValueChange} />
+            
+            vanilla</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="chocholate"
+                checked={selectedOption==="chocholate"}
+                onChange={onValueChange} />
+            chocholate</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="nutella" 
+                checked={selectedOption==="nutella"}
+                onChange={onValueChange} />
+              
+            nutella</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="red satin"
+                checked={selectedOption==="red satin"}
+                onChange={onValueChange} />
+              red satin</label><br/>
+            <br/>
+            <br/>
+            
+            <label style={{fontWeight:'bold'}}>Message</label><br/>
+            <textarea>Message to us</textarea><br/>
+            
+            <button onClick={()=>Counter>1?setCounter(Counter-1):setCounter(1)}>-</button>
+            <input className='inputbutton' value={Counter} ></input>
+             
+ 
+            <button onClick={()=>setCounter(Counter+1)}>+</button><br/>
+            <button>Add to cart</button>
           </div>
           
     </div>
--- src/pages/OrderCupCakes.js
+++ src/pages/OrderCupCakes.js
@@ -1,8 +1,8 @@
 import Layout from "../components/Layout";
 import Footer from "../components/Footer"
+import { useState } from 'react';
 import "./OrderCupCakes.css"
-import { useLocation } from "react-router-dom";
-import {useState} from "react-router-dom";    
+import { useLocation } from "react-router-dom";   
 import Img1 from '../Images/CupCake1.png';
 import Img2 from '../Images/CupCake2.png';
 import Img3 from '../Images/CupCake3.png';
@@ -20,7 +20,10 @@ import Img14 from '../Images/CupCake14.png';
 import Img15 from '../Images/CupCake15.png';
 
 
-export default function OrderCupCakes(props) { 
+export default function OrderCupCakes(props) {
+  const[selectedOption,setSelectedOption]=useState("vanilla");
+  const[selectedOption2,setSelectedOption2]=useState("10"); 
+    const [Counter,setCounter]=useState(1)
   const location=useLocation();
   const data =location.state;
   const {id,type,name,price}=data;
@@ -28,6 +31,14 @@ export default function OrderCupCakes(props) {
     Img11,Img12,Img13,Img14,Img15]
   let src=imgarray[id-1];
  
+  function onValueChange(event)
+  {
+    setSelectedOption(event.target.value)
+  }
+  function onValueChange2(event)
+  {
+    setSelectedOption(event.target.value)
+  }
     return(
         <div>
         <Layout />
@@ -36,7 +47,7 @@ export default function OrderCupCakes(props) {
             <img 
               src={src}
               alt="cake"
-              style={{padding:20 ,maxWidth:500} }
+              style={{padding:20, maxWidth:750} }
             >
             </img>
             <h3>Description</h3>
@@ -45,13 +56,77 @@ export default function OrderCupCakes(props) {
             <h2>{name}</h2>
             <h3>{price}</h3>
             <h4> Choose box size</h4>
-            1 cupcake
+            <label>
+              <input 
+                type='radio' 
+                value="1"
+                  checked={selectedOption2==="10"}
+                onChange={onValueChange2} />
+            
+            1 cupcake</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="6"
+                checked={selectedOption2==="6"}
+                onChange={onValueChange2} />
+            6 cupcakes</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="12" 
+                checked={selectedOption2==="12"}
+                onChange={onValueChange2} />
+            12 portions</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="12" 
+                checked={selectedOption2==="25"}
+                onChange={onValueChange2} />
+            25 portions</label><br/>
             <h4> Choose filling</h4>
-            vanillia
-            <h4>Message to us</h4>
-            <input></input>
-            Number of boxes:<input></input>
-            <button>Place in cart</button>
+            <label>
+              <input 
+                type='radio' 
+                value="vanilla"
+                  checked={selectedOption==="vanilla"}
+                onChange={onValueChange} />
+            
+            vanilla</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="chocholate"
+                checked={selectedOption==="chocholate"}
+                onChange={onValueChange} />
+            chocholate</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="nutella" 
+                checked={selectedOption==="nutella"}
+                onChange={onValueChange} />
+              
+            nutella</label><br/>
+            <label>
+              <input 
+                type='radio' 
+                value="red satin"
+                checked={selectedOption==="red satin"}
+                onChange={onValueChange} />
+              red satin</label><br/>
+            <br/>
+            <br/>
+            <label style={{fontWeight: 'bold'}}>Message</label>
+            <textarea>Message to us</textarea><br/>
+            
+            <button onClick={()=>Counter>1?setCounter(Counter-1):setCounter(1)}>-</button>
+            <input className='inputbutton' value={Counter}  ></input>
+             
+ 
+            <button onClick={()=>setCounter(Counter+1)}>+</button><br/>
+            <button>Add to cart</button>
           </div>
           
     </div>
